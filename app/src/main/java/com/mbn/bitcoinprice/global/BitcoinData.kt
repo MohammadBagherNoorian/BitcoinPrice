dc98184c808cf509c84d4200ba15e44ba04d9c28
Mohammad bagher noorian <mbndeveloper1994@gmail.com>
2021-09-23 10:44:17 +0330

    delete boilerplate code

--- app/src/main/java/com/mbn/bitcoinprice/global/BitcoinData.kt
+++ app/src/main/java/com/mbn/bitcoinprice/global/BitcoinData.kt
@@ -1,5 +1,6 @@
 package com.mbn.bitcoinprice.global
 
+import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.ComponentName
@@ -17,55 +18,64 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
-class BitcoinData (mcontext: Context){
-    private val context=mcontext
+class BitcoinData(mcontext: Context) {
+    private val context = mcontext
     private var result = MutableLiveData<BTCModel>()
-    fun getResult():LiveData<BTCModel>{
+    fun getResult(): LiveData<BTCModel> {
         return result
     }
-    @Synchronized fun fetchData() {
+
+    @Synchronized
+    fun fetchData() {
         val res: MutableLiveData<Number> = MutableLiveData()
         val disposables = CompositeDisposable()
         val repo = BitCoinRepository()
         //for show Loading when in progress
-        setWidgetViewConfig(context,context.getString(R.string.loading_text),context.getString(R.string.loading_text))
+        setWidgetViewConfig(
+            context,
+            context.getString(R.string.loading_text),
+            context.getString(R.string.loading_text)
+        )
         //for get more Decimal
-        disposables.add(repo.executeGetValueOfBitCoinPerOneUSD("USD", 1000)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe( {
-                res.value=it
-                Log.e("result1",res.value.toString())
-                convertUsdToBTCPrice(context,res.value.toString())
-            },{
-                    t: Throwable? ->
-                convertUsdToBTCPrice(context,"0")
+        disposables.add(
+            repo.executeGetValueOfBitCoinPerOneUSD("USD", 1000)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    res.value = it
+                    Log.e("result1", res.value.toString())
+                    convertUsdToBTCPrice(context, res.value.toString())
+                }, { t: Throwable? ->
+                    convertUsdToBTCPrice(context, "0")
 
-            })
+                })
         )
 
     }
-    private fun convertUsdToBTCPrice(context: Context,thousdandUsd:String){
-        //Formula is : BtcPrice=1/thousdandUsd
-        if (thousdandUsd!="0"){
-            val btcPrice=(1/thousdandUsd.toDouble())*1000
-            Log.e("resultBtc",String.format("%.2f", btcPrice))
 
-            result.value= BTCModel(0,String.format("%.2f", btcPrice)).also {
+    private fun convertUsdToBTCPrice(context: Context, thousdandUsd: String) {
+        //Formula is : BtcPrice=1/thousdandUsd
+        if (thousdandUsd != "0") {
+            val btcPrice = (1 / thousdandUsd.toDouble()) * 1000
+            val btcPricesTwoDecimal = String.format("%.2f", btcPrice)
+            result.value = BTCModel(0, btcPricesTwoDecimal).also {
                 App.appDataBase?.provideDao()?.insertBitcoinPrice(it)
             }
-            setWidgetViewConfig(context,String.format("%.2f", btcPrice)+"$", getCurrentTime())
-        }else{
-            Log.e("resultBtc","from DB")
+            setWidgetViewConfig(
+                context, "$btcPricesTwoDecimal$", getTimeByTimeStamp(System.currentTimeMillis())
+            )
+        } else {
             App.appDataBase?.provideDao()?.getLastBTCPrice()?.blockingGet().apply {
-                if (this!=null){
-                    result.value= BTCModel(0,this.price,this.time)
-                    setWidgetViewConfig(context,this.price, getTimeByTimeStamp(this.time))
-                }else{
-                    result.value= BTCModel(0,context.getString(R.string.loading_text))
-                    setWidgetViewConfig(context,context.getString(R.string.loading_text), context.getString(
-                        R.string.loading_text
-                    ))
+                if (this != null) {
+                    result.value = BTCModel(0, this.price, this.time)
+                    setWidgetViewConfig(context, this.price, getTimeByTimeStamp(this.time))
+                } else {
+                    result.value = BTCModel(0, context.getString(R.string.loading_text))
+                    setWidgetViewConfig(
+                        context, context.getString(R.string.loading_text), context.getString(
+                            R.string.loading_text
+                        )
+                    )
                 }
 
             }
@@ -73,11 +83,12 @@ class BitcoinData (mcontext: Context){
 
     }
 
-    private fun setWidgetViewConfig(context: Context, price:String, lastupdate:String){
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun setWidgetViewConfig(context: Context, price: String, lastupdate: String) {
 
         val views = RemoteViews(context.packageName, R.layout.bitcoin)
         views.setTextViewText(R.id.appwidget_text, price)
-        views.setTextViewText(R.id.last_update,lastupdate)
+        views.setTextViewText(R.id.last_update, lastupdate)
         val intent = Intent(context, BitcoinWidget::class.java)
         intent.action = "ManulaClick"
         val pendingIntent = PendingIntent.getBroadcast(
