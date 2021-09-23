fe24266490f12fca98e3d3ec4dffb630460e5c2f
Mohammad bagher noorian <mbndeveloper1994@gmail.com>
2021-09-23 10:53:33 +0330

    write comment and rename file

--- app/src/main/java/com/mbn/bitcoinprice/global/TimeExtention.kt
+++ app/src/main/java/com/mbn/bitcoinprice/global/TimeExtention.kt
@@ -3,18 +3,14 @@ package com.mbn.bitcoinprice.global
 import java.text.SimpleDateFormat
 import java.util.*
 
-
-
-fun getCurrentTime(): String {
-    val dateObj: Date = Calendar.getInstance().time
-    return SimpleDateFormat("K:mm:ss a", Locale.US).format(dateObj).toString()
-}
-
-fun getTimeByTimeStamp(time:Long): String {
+//get time (hh:mm:ss) by timestamp to show lastupdate
+fun getTimeByTimeStamp(time: Long): String {
     val dateObj = Date(time)
     return SimpleDateFormat("K:mm:ss a", Locale.US).format(dateObj).toString()
 }
- fun getDateTime(s: Long): String {
+
+//get date by timestamp (MM/dd/yyyy) To categorize requests by date
+fun getDateTime(s: Long): String {
     return try {
         val sdf = SimpleDateFormat("MM/dd/yyyy")
         val netDate = Date(s)
