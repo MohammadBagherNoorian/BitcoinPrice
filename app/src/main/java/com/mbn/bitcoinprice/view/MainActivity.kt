ae711629b7c06a5cb7c1fa44c6843ab2363db11f
Mohammad bagher noorian <mbndeveloper1994@gmail.com>
2021-09-25 18:57:42 +0330

    Change MainActivity.kt base Dagger and inject fields

--- app/src/main/java/com/mbn/bitcoinprice/view/MainActivity.kt
+++ app/src/main/java/com/mbn/bitcoinprice/view/MainActivity.kt
@@ -1,23 +1,28 @@
 package com.mbn.bitcoinprice.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.mbn.bitcoinprice.global.App
 import com.mbn.bitcoinprice.global.BitcoinData
 import com.mbn.bitcoinprice.R
 import com.mbn.bitcoinprice.adapter.HistoryAdapter
+import com.mbn.bitcoinprice.db.BitcoinDatabase
 import com.mbn.bitcoinprice.global.getTimeByTimeStamp
+import com.mbn.bitcoinprice.network.BitCoinService
+import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-class MainActivity : AppCompatActivity() {
+import javax.inject.Inject
 
+class MainActivity : DaggerAppCompatActivity() {
+
+    @Inject lateinit var bitCoinService: BitCoinService
+    @Inject lateinit var bitcoinData: BitcoinData
+    @Inject lateinit var bitcoinDatabase: BitcoinDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btcDataAdapter= BitcoinData(this)
-        btcDataAdapter.fetchData()
-        btcDataAdapter.getResult().observe(this, Observer {
+
+        bitcoinData.fetchData(bitCoinService)
+        bitcoinData.getResult().observe(this, {
             main_price_text.text=it.price
             main_last_update.text= getTimeByTimeStamp(it.time)
             showHistoryofBitcoin()
@@ -25,14 +30,14 @@ class MainActivity : AppCompatActivity() {
         })
 
         refresh.setOnClickListener {
-            btcDataAdapter.fetchData()
+            bitcoinData.fetchData(bitCoinService)
         }
 
 }
 
-    fun showHistoryofBitcoin(){
+    private fun showHistoryofBitcoin(){
 
-        App.appDataBase?.provideDao()?.getLast4Average()?.blockingGet().also {
+        bitcoinDatabase.provideDao().getLast4Average().blockingGet().also {
             val adapter= it?.let { it1 -> HistoryAdapter(it1) }
             val linearlayout = LinearLayoutManager(this)
             history_rv.adapter=adapter
